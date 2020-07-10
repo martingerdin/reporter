b1d6301ffa44c73584921acb747e7418d2921735
Martin Gerdin Warnberg <martingerdin@gmail.com>
2020-07-10 10:16:59 +0200

    Remove custom utility functions from package

--- R/package.R
+++ R/package.R
@@ -1,17 +1,4 @@
 #' @import assertthat
+#' @importFrom dplyr `%>%`
 NULL
-
-test_params <- function() {
-    params <- list(
-        codebook = rio::import(system.file("extdata", "codebook.csv", package = "reporter")),
-        faulty.codebook = rio::import(system.file("extdata", "faulty-codebook.csv", package = "reporter")),
-        dataset = rio::import(system.file("extdata", "codebook.csv", package = "reporter"))
-    )
-    return (params)
-}
-
-stop_wrap <- function(...) {
-    stop (stringr::str_wrap(paste0(...)))
-}
-
 
