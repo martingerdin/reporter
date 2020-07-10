f4e5c62015fa907d6a0346a4da505a6bce0fb188
Martin Gerdin Warnberg <martingerdin@gmail.com>
2020-07-10 10:18:39 +0200

    Update import tests

--- tests/testthat/test-import.R
+++ tests/testthat/test-import.R
@@ -1,8 +1,11 @@
-context("import_codebook")
-
-get_dataset <- function(x) rio::import(system.file("extdata", "dataset.csv", package = "reporter"))
+context("Import functions")
 
 test_that("import_codebook throws errors on faulty codebook", {
     path <- system.file("extdata", "faulty-codebook.csv", package = "reporter")
     expect_error(import_codebook(path), "The file that you")
+})
+
+test_that("import_dataset returns tibble", {
+    path <- system.file("extdata", "dataset.csv", package = "reporter")
+    expect_s3_class(import_dataset(path), "tbl_df")
 })
