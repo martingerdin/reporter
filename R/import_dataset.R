d6a9665416c21cafa3688b0d34dabab634655c4b
Martin Gerdin Warnberg <martingerdin@gmail.com>
2020-07-10 10:18:54 +0200

    Add import dataset function

---
+++ R/import_dataset.R
@@ -0,0 +1,17 @@
+#' Import dataset
+#'
+#' Imports a dataset
+#' @param path Character vector of length 1. The path to the
+#'     dataset. Defaults to `"./dataset.csv"`, i.e. it looks for a
+#'     file called `dataset.csv` in the current working directory.
+#' @return A tibble.
+#' @examples
+#' \dontrun{
+#' import_dataset()
+#' }
+#' @export
+import_dataset <- function(path) {
+    assert_path()
+    dataset <- rio::import(path) %>% dplyr::as_tibble()
+    return (dataset)
+}
