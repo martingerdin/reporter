f44e76b6d2c61f47cee3bbb741f075a1bf8e2d92
Martin Gerdin Warnberg <martingerdin@gmail.com>
2020-07-10 10:23:14 +0200

    Add test for correct codebook class

--- tests/testthat/test-import.R
+++ tests/testthat/test-import.R
@@ -1,5 +1,10 @@
 context("Import functions")
 
+test_that("import_codebook returns reporter_codebook", {
+    path <- system.file("extdata", "codebook.csv", package = "reporter")
+    expect_s3_class(import_codebook(path), "reporter_codebook")
+})
+
 test_that("import_codebook throws errors on faulty codebook", {
     path <- system.file("extdata", "faulty-codebook.csv", package = "reporter")
     expect_error(import_codebook(path), "The file that you")
@@ -9,6 +14,7 @@ test_that("import_dataset returns tibble", {
     path <- system.file("extdata", "dataset.csv", package = "reporter")
     expect_s3_class(import_dataset(path), "tbl_df")
 })
+
 test_that("import_dataset returns reporter_dataset", {
     path <- system.file("extdata", "dataset.csv", package = "reporter")
     expect_s3_class(import_dataset(path), "reporter_dataset")
